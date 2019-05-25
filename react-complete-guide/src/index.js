cc574e3379656bbd7f2536856db3676c811a14df
dstrauss <fmdenver@gmail.com>
2019-05-25 19:26:22 -0400

    starting lifecycle hooks

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './Person/Person.css';
-import App from './App';
+import './components/Persons/Person/Person.css';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App appTitle="Person Manager" />, document.getElementById('root'));
 registerServiceWorker();
